f8377bd30fec0e61808d7dbe4647c340681d80a5
Jose <jose@support.webartdesign.com.au>
2019-08-04 06:19:18 +0800

    Updates

---
+++ precache-manifest.cd8cb6772f41369b167d915e8c261538.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7651a148165e9f9cbc7b997d4821b355",
+    "url": "/teewao-birthday/index.html"
+  },
+  {
+    "revision": "2bf04b5a0f6b186bf325",
+    "url": "/teewao-birthday/static/css/main.becce491.chunk.css"
+  },
+  {
+    "revision": "d93fae47310e6c9c72a0",
+    "url": "/teewao-birthday/static/js/2.f6df4593.chunk.js"
+  },
+  {
+    "revision": "2bf04b5a0f6b186bf325",
+    "url": "/teewao-birthday/static/js/main.a9287190.chunk.js"
+  },
+  {
+    "revision": "31a7eff51f5cf5d46c14",
+    "url": "/teewao-birthday/static/js/runtime~main.d5254622.js"
+  },
+  {
+    "revision": "3a24b3df61440de35c8862dc6491bfed",
+    "url": "/teewao-birthday/static/media/grandpic.3a24b3df.jpg"
+  },
+  {
+    "revision": "9edbdc9b3e95665d9b5f73d43938082f",
+    "url": "/teewao-birthday/static/media/ring.9edbdc9b.jpg"
+  },
+  {
+    "revision": "d763a272a1a46e18c579b2e7fa545211",
+    "url": "/teewao-birthday/static/media/startcity.d763a272.jpg"
+  },
+  {
+    "revision": "6bf64772d2379e11f5da9d15ab83b210",
+    "url": "/teewao-birthday/static/media/teewaoSmile.6bf64772.jpg"
+  }
+]);
